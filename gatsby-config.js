402a5f04c35b1dc4637dad926a3953b2a1e9296a
webskitters <webskitters@webskitterss-iMac.local>
2021-03-05 11:00:22 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "My Gatsby Site",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-sass",
+    "gatsby-plugin-gatsby-cloud",
+  ],
+};
